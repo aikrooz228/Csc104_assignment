1f8e606831386634f326b5eca72d43c534314126
aikrooz228 <aikrooz228@gmail.com>
2025-06-14 03:30:42 -0700

    it working the first

---
+++ pages/saving.py
@@ -0,0 +1,17 @@
+import sys 
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
+
+
+from saving_account import SavingAccount
+
+import streamlit as st
+from saving_account import SavingAccount
+
+st.title("Saving Account")
+
+
+
+
+
+st.write("This is my saving account")
